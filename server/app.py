59f35d4e145f764dc8c5e2e7f755efb0c966b805
KomfortKimko <komfortkimk@gmail.com>
2024-10-09 11:20:50 +0300

    Flask-RESTful-CR-lab

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from models import db, Plant
 
 from models import db, Plant
 
@@ -17,10 +18,50 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_dict_list = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            plant_dict_list,
+            200,
+        )
+
+        return response
+    
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        plant_dict = new_plant.to_dict()
+        response = make_response(
+            plant_dict,
+            201,
+        )
+
+        return response 
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            plant_dict,
+            200,
+        )
+
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
